339e192d55417596a7e139dea0c1417ace03d8a8
mihirWeb <mihirpratapsingh166@gmail.com>
2024-06-30 12:13:02 +0530

    routes added for user controller end point

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, logOutUser, loginUser, refreshAccessToken, registerUser, updateAccountDetail, updateCoverImage } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, logOutUser, loginUser, refreshAccessToken, registerUser, updateAccountDetail, updateCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,10 +25,13 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser); // auth middleware added
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current-user").post(verifyJWT, getCurrentUser);
-router.route("/update-account-detail").post(verifyJWT, updateAccountDetail);
-router.route("/update-avatar").post(verifyJWT, upload.single("avatar"), updateAccountDetail);
-router.route("/update-cover-image").post(verifyJWT, upload.single("coverImage") , updateCoverImage);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-detail").patch(verifyJWT, updateAccountDetail);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAccountDetail);
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage") , updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // we used : here bcz in our controller we fetched username from params and value after ":" is fetched in params
+router.route("/history").get(verifyJWT, getUserChannelProfile)
 
 
 export default router;
